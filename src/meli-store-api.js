8eaf689943bb92b48d7f44db512e0f6e1e861cb0
Lion <breakermoob@hotmail.com>
2022-05-02 16:49:12 -0500

    layout

--- src/meli-store-api.js
+++ src/meli-store-api.js
@@ -1,1 +1,2 @@
-export { getProductsByKeyword } from "./services/products";
+export { getProductsByKeyword, getProductById, getProducts, setProducts } from "./services/products";
+export { getCarProducts, addProductToCar } from "./services/shopping-car";
--- src/services/products.js
+++ src/services/products.js
@@ -1,8 +1,31 @@
+import { BehaviorSubject } from "rxjs";
+
 const URL_MERCADOAPI = "https://api.mercadolibre.com";
 const URL_GET_SEARCH = "/sites/MCO/search?q=";
+const URL_GET_PRODUCT_BY_ID = "/items/";
 
-export const getProductsByKeyword = async (keyword) => {
-  const response = await fetch(`${URL_MERCADOAPI}${URL_GET_SEARCH}${keyword}`);
+const products = new BehaviorSubject([]);
+
+const getByUrl = async (url) => {
+  const response = await fetch(url);
   const data = await response.json();
   return data;
 };
+
+export const getProducts = () => {
+  return products;
+};
+
+export const setProducts = (data) => {
+  products.next(data);
+};
+
+export const getProductsByKeyword = async (keyword) => {
+  return await getByUrl(`${URL_MERCADOAPI}${URL_GET_SEARCH}${keyword}`);
+};
+
+export const getProductById = async (id) => {
+  return await getByUrl(`${URL_MERCADOAPI}${URL_GET_PRODUCT_BY_ID}${id}`);
+};
+
+
